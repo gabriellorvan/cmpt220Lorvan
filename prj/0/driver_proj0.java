57b8a1b2061a6f9f2a4d35b1f02770b23b08cfbe
gabriellorvan <gabriel.lorvan1@marist.edu>
2017-09-01 18:33:40 -0400

    Project 0

---
+++ prj/0/driver_proj0.java
@@ -0,0 +1,10 @@
+package prj0Lorvan;
+
+public class driver_proj0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
